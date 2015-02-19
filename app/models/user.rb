27a1ff066676e71821e824e30c3e2dce0718dfd9
bibiana <bcristofol@gmail.com>
2015-02-19 14:51:54 +0000

    fixed typo and fb sign up now works well

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,19 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:facebook]
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, iud: auth.uid).first_or_create do |user|
+    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
-      user.mage = auth.info.image
+      # user.name = auth.info.name
+      # user.image = auth.info.image
     end
   end
 
-  # def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["raw_info"]
-  #       user.email = data["email"] if user.email.blank?
-  #     end
-  #   end
-  # end
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
 end
