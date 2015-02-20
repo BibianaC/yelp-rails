70c26bf26ff7fefd90cad5ac62704f76a40d33fb
bibiana <bcristofol@gmail.com>
2015-02-20 21:22:59 +0000

    Added average rating functionality

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -5,6 +5,8 @@ feature 'reviewing' do
  before do
     user = User.new(email: 'test@example.com', password: 'testtest', password_confirmation: 'testtest')
     user.save
+    user2 = User.new(email: 'test2@example.com', password: 'testtest', password_confirmation: 'testtest')
+    user2.save
     user.restaurants.create name: 'KFC'
   end
 
@@ -14,4 +16,14 @@ feature 'reviewing' do
     expect(current_path).to eq '/restaurants'
     expect(page).to have_content('so so')
   end
+
+  scenario 'displays an average rating for all reviews' do
+    sign_in('test@example.com', 'testtest')
+    leave_review('So so', '3')
+    click_link 'Sign out'
+    sign_in('test2@example.com', 'testtest')
+    leave_review('Great', '5')
+    expect(page).to have_content('Average rating: 4')
+  end
+
 end
